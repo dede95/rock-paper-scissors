1ec5bc37df208f9c86806ab3d82190b7ceeaa627
dede95 <29232994+dede95@users.noreply.github.com>
2024-09-21 01:48:26 +0100

    Add user prompt to add choice for game

--- rps-game.js
+++ rps-game.js
@@ -4,10 +4,32 @@ function getComputerChoice() {
 
     // times random (0 to 1) by the length of array and get floor, 
     //giving the values, 0,1 or 2 - represents the choices!
-    let cpChoice = Math.floor(Math.random() * choices.length)
+    let cpChoice = Math.floor(Math.random() * choices.length);
 
-    return choices[cpChoice]
+    return choices[cpChoice];
 }
 
 // console.log(getComputerChoice())
+
+function getHumanChoice() {
+
+    let huChoice = parseInt(prompt('Rock (1), Paper (2), or Scissors (3)? Please choose the corresponding number of your choice:'));
+
+    if (huChoice === 0) {
+        return choices[0];
+    }
+    else if (huChoice === 1) {
+        return choices[1];
+    }
+    else if (huChoice === 2) {
+        return choices[2];
+    }
+    else {
+        return console.log("Enter a number!");
+    }
+
+}
+
+// console.log(getHumanChoice())
+
 
