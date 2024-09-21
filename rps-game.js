6bff16eb05e56ecabcf801f8f6cd01bda032631a
dede95 <29232994+dede95@users.noreply.github.com>
2024-09-21 01:34:20 +0100

    Add computer choice for game

--- rps-game.js
+++ rps-game.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+const choices = ['rock', 'paper', 'scissors']
+
+function getComputerChoice() {
+
+    // times random (0 to 1) by the length of array and get floor, 
+    //giving the values, 0,1 or 2 - represents the choices!
+    let cpChoice = Math.floor(Math.random() * choices.length)
+
+    return choices[cpChoice]
+}
+
+// console.log(getComputerChoice())
+
